a273aa0fb62e3ce8dfa36b4d9c6656babaedd226
imunreal7 <dubeyaman174@gmail.com>
2025-04-17 10:26:40 +0530

    Added landing page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div style={{ padding: "2rem", textAlign: "center" }}>
             <h1>Welcome to Secret Echo 👋</h1>
-            <p>This is your landing page.</p>
+            <p>This is a simple chat app with AI powered responses</p>
             <div style={{ marginTop: "1rem" }}>
                 <Link href="/login">Go to Login</Link> | <Link href="/signup">Signup</Link>
             </div>
