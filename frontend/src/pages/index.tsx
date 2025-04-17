362f39035325580cab3b1e45400be614e736815e
imunreal7 <dubeyaman174@gmail.com>
2025-04-17 10:25:43 +0530

    Add landing page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+// frontend/src/pages/index.tsx
+import React from "react";
+import Link from "next/link";
+
+const Home = () => {
+    return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+            <h1>Welcome to Secret Echo 👋</h1>
+            <p>This is your landing page.</p>
+            <div style={{ marginTop: "1rem" }}>
+                <Link href="/login">Go to Login</Link> | <Link href="/signup">Signup</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
+
